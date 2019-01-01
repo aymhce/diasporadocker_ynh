498d42ea229e800a8994e42acf0dd8252884a815
Développeur libre <aymhce@gmail.com>
2019-01-01 11:16:27 +0100

    Update run.sh
    go to version 0.7.8.0

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -2,7 +2,7 @@
 
 source /usr/share/yunohost/helpers
 
-[ "$architecture" == "amd64" ] && export version_diaspora=0.7.7.1
+[ "$architecture" == "amd64" ] && export version_diaspora=0.7.8.0
 [ -z $version_diaspora ] && ynh_die "Sorry, your $architecture architecture is not supported ..."
 
 db_pwd=$(ynh_app_setting_get $app dbpwd)
